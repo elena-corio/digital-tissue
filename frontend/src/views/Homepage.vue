0c448ea671eea02ef81e23837e139d57a8dc072b
elena-code-star <elena.corio@students.iaac.net>
2026-02-04 23:10:26 +0100

    Add views placeholders

---
+++ frontend/src/views/Homepage.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="page-container">
+    <h1>Homepage</h1>
+  </div>
+</template>
---
+++ frontend/src/views/Projects.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="page-container">
+    <h1>Projects</h1>
+  </div>
+</template>
