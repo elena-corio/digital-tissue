a209db2728ac765c725940147202036889ab6090
elena-code-star <elena.corio@students.iaac.net>
2026-02-08 20:23:32 +0100

    Create model Config file for holding project id and model id

---
+++ frontend/src/config/modelConfig.js
@@ -0,0 +1,13 @@
+// modelConfig.js
+// Store project and model IDs for each viewer
+
+export const viewerModels = [
+  {
+    projectId: 'f619bcc233',
+    modelId: '2d6cffa5b8',
+  },
+  {
+    projectId: 'f619bcc233',
+    modelId: '2d6cffa5b8', // Replace with second modelId if needed
+  }
+];
