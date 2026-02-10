5ae68d82d00150a9038902a937da8bb1525b47f3
elena-code-star <elena.corio@students.iaac.net>
2026-02-10 21:21:10 +0100

    remove duplicated html body style

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -12,12 +12,5 @@ import Header from '@/components/layout/Header.vue';
 </script>
 
 <style>
-html, body, #app {
-  width: 100vw;
-  height: 100vh;
-  margin: 0;
-  padding: 0;
-  overflow: hidden !important;
-  box-sizing: border-box;
-}
+
 </style>
