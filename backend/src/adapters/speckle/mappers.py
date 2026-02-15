65e0a8c5d9878f12e50ae62524b0def11f52c606
elena-code-star <elena.corio@students.iaac.net>
2026-02-15 23:28:14 +0100

    Add facades to model

--- backend/src/adapters/speckle/mappers.py
+++ backend/src/adapters/speckle/mappers.py
@@ -1,3 +1,4 @@
+from domain.model.elements import Facade
 from domain.model.enum import ProgramType
 from domain.model.model import OpenSpace, Unit
 
@@ -19,6 +20,19 @@ def speckle_to_open_space(speckle_obj) -> OpenSpace:
         cluster_id=speckle_obj.properties["cluster_id"],
         level=speckle_obj.properties["level"],
         area=round(speckle_obj.area, 2),
+        speckle_type=speckle_obj.speckle_type,
+        geometry=speckle_obj
+    )
+    
+
+def speckle_to_facade(speckle_obj) -> Facade:
+    """Convert Speckle object to domain Facade."""
+    return Facade(
+        cluster_id=speckle_obj.properties["cluster_id"],
+        level=speckle_obj.properties["level"],
+        material=speckle_obj.properties["material"],
+        area=round(speckle_obj.area, 2),
+        thickness=0,
         speckle_type=speckle_obj.speckle_type,
         geometry=speckle_obj
     )
--- backend/src/adapters/speckle/receive_data.py
+++ backend/src/adapters/speckle/receive_data.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 from typing import Any, Dict
 from specklepy.api import operations
 
-from adapters.speckle.mappers import speckle_to_open_space, speckle_to_unit
+from adapters.speckle.mappers import speckle_to_facade, speckle_to_open_space, speckle_to_unit
 from domain.model.model import Model
     
 def receive_data(version, transport):
@@ -14,7 +14,10 @@ def receive_data(version, transport):
     open_spaces_collection = next(collection for collection in data.elements if collection.name == "OPEN_SPACES")
     green_spaces = [speckle_to_open_space(gs) for gs in open_spaces_collection.elements]
     
+    facades_collection = next(collection for collection in data.elements if collection.name == "FACADES")
+    facades = [speckle_to_facade(f) for f in facades_collection.elements]
+    
     levels = set(unit.level for unit in units)
     clusters = set(unit.cluster_id for unit in units)
     
-    return Model(units=units, open_spaces=green_spaces, levels=list(levels), clusters=list(clusters))
+    return Model(units=units, open_spaces=green_spaces, facades=facades, levels=list(levels), clusters=list(clusters))
