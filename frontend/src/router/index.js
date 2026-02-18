ca1b560d54228c82922acfe2b5a7047f2058670d
elena-code-star <elena.corio@students.iaac.net>
2026-02-18 14:33:13 +0100

    Switch to hash mode

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 import Homepage from '@/views/Homepage.vue'
 import Workspace from '@/views/Workspace.vue'
@@ -45,7 +45,7 @@ const routes = [
 
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes
 })
 
